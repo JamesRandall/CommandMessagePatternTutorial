8a8e6669027e88a46bd8dc73e25dfb3458b0ae30
James Randall <james@accidentalfish.com>
2017-11-28 08:36:59 +0000

    Corrected bad name and added product controller

---
+++ Part1a/OnlineStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Model;
+using OnlineStore.Services;
+
+namespace OnlineStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<StoreProduct> Get(Guid id)
+        {
+            return await _productService.GetAsync(id);
+        }
+    }
+}
---
+++ Part1b/OnlineStore.Api/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using AzureFromTheTrenches.Commanding.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Store.Commands;
+using Store.Model;
+
+namespace OnlineStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly ICommandDispatcher _dispatcher;
+
+        public ProductController(ICommandDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<StoreProduct> Get(Guid id)
+        {
+            return await _dispatcher.DispatchAsync(new GetStoreProductQuery
+            {
+                ProductId = id
+            });
+        }
+    }
+}
